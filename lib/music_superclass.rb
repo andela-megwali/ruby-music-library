c5a4a1a9d598ffda740229ce794438de2f0c93f6
micEnigma <mic.excel@gmail.com>
2016-07-27 12:17:40 +0100

    Add music_superclass.rb to lib folder

---
+++ lib/music_superclass.rb
@@ -0,0 +1,23 @@
+class Music
+	@@all = []
+  attr_accessor :name, :artist, :genre, :songs
+	
+  def self.all
+    @@all
+  end
+
+  def save
+  	@@all << self
+  end
+
+  def self.destroy_all
+  	@@all = []
+  end
+
+  def self.create(name)
+  	self.new(name).save
+    @@all[-1]
+  end
+
+end
+
