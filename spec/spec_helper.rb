efdf8739c95d3ecb0e92ab7e22f0b0e78aa636ac
andela-megwali <michael.egwali@andela.com>
2016-08-07 22:16:11 +0100

    Update spec_helper.rb for code coverage test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
-require_relative "../config/environment.rb"
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+require_relative "../config/environment.rb"
 
 RSpec.configure do |config|
   config.order = :default
